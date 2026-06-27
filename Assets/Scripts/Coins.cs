567fc62fefda117be124c30575a2d94bdd05f625
Neirizabagas <neirizabagas@gmail.com>
2026-06-27 13:52:11 +0700

    Update 2
    Main Target:
    -Player Locomotive: Done
    -Camera: Done
    -Collectible Item: Done
    -Collection Mechanic: Done
    -Scoring: Done
    -Item Dissapearance: Done
    -UI Display: Done
    -Obstacle: Done
    -Android Build: Done
    
    Bonus Target:
    -Sfx: Done
    -Timer: Done
    -Variation Item Type: Done
    -Particle Effect: Done
    -High Score System: Done

--- Assets/Scripts/Coins.cs
+++ Assets/Scripts/Coins.cs
@@ -3,13 +3,51 @@ using UnityEngine;
 
 public class Coins : MonoBehaviour
 {
-    public int value = 1; // The value of the coin
-    
+    public enum ItemTypes { Coin, BonusCoin, TrapCoin }
+
+    [Header("Item Type")]
+    [SerializeField] private ItemTypes itemType;
+
+    [SerializeField] private int basicValue = 1; // The value of the coin
+    [SerializeField] private int bonusValue = 5; // The value of the bonus coin
+    [SerializeField] private int trapValue = -1; // The value of the trap coin
+
+
+    [Header("Sfx & Vfx")]
+    [SerializeField] private AudioClip collectSfx;
+    [SerializeField] private ParticleSystem collectVfx;
+
     public static event Action<int> OnItemCollected;
     
     public void Collect()
     {
-        OnItemCollected?.Invoke(value);
+        int score = 0;
+
+        switch (itemType)
+        {
+            case ItemTypes.Coin:
+                score = basicValue;
+                break;
+            case ItemTypes.BonusCoin:
+                score = bonusValue;
+                break;
+            case ItemTypes.TrapCoin:
+                score = trapValue;
+                break;
+        }
+
+        if (collectSfx != null)
+        {
+            AudioSource.PlayClipAtPoint(collectSfx, transform.position);
+        }
+
+        if (collectVfx != null)
+        {
+            ParticleSystem effect = Instantiate(collectVfx, transform.position, Quaternion.identity);
+            Destroy(effect.gameObject, effect.main.duration); // Destroy the particle system after its duration
+        }
+
+        OnItemCollected?.Invoke(score);
         Destroy(gameObject); // Destroy the coin object after collection
     }
 }
